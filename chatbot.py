25a0bf73d19cdfd5f332b403828085a926d03260
Suvie_Suuwethaan <suvie616@gmail.com>
2026-06-05 06:02:37 -0700

    First commit

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,26 @@
 from google import genai
 from flask import Flask,request,jsonify
-client = genai.Client(api_key="AIzaSyBKkXfm5vB11M9BRoevTOWWft83mTKYTME")
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "exit":
-        break
+from flask_cors import CORS
+app = Flask(__name__)
+
+
+
+
+CORS(app,resources={r"/*": {"origins": "*"}})
+client = genai.Client(api_key="AQ.Ab8RN6J5Sf23alCt_8OH448--JDi-5p5wcDs838QA0HG7m4pCA")
+
+@app.route("/summary",methods=["POST"])
+def summary():
+    data = request.get_json()
+    user_input = data.get("text")
+    print("Received text:", user_input)
+
     response = client.models.generate_content(
         model="gemini-3.5-flash",
-        contents=user_input
+        contents=("Give a summary of this input:", user_input)
     )
     print("AI:", response.text)
-    print('-'*100)
+
+    return jsonify({"summary": response.text})
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=5000)
--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const textarea = document.querySelector("#noteInput");
 const button = document.querySelector("#generateBtn");
 const paragraph = document.querySelector("#infoText");
 
-button.addEventListener("click", () => {
-  paragraph.textContent = textarea.value;
+button.addEventListener("click",async (e) => {
+   const text = textarea.value;
+ try {
+   const req = await fetch ("http://127.0.0.1:5000/summary", { 
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ text})
+  });
+  const data = await req.json();
+  paragraph.textContent = data.summary;
+  paragraph.classList.remove("hidden");
+
+  } catch (error) {
+    console.log("Fetch error:", error);
+    paragraph.textContent = error
+  }
 });
