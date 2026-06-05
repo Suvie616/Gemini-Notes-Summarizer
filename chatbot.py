ebc11b169be0bed570c37e294291e7493645ae5f
Suvie_Suuwethaan <suvie616@gmail.com>
2026-06-05 06:40:27 -0700

    edwdwu

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,5 @@
 from google import genai
 from flask import Flask,request,jsonify
-<<<<<<< HEAD
 from flask_cors import CORS
 app = Flask(__name__)
 
@@ -8,7 +7,7 @@ app = Flask(__name__)
 
 
 CORS(app,resources={r"/*": {"origins": "*"}})
-#client = genai.Client(api_key="Your API Key")
+client = genai.Client(api_key="AQ.Ab8RN6LozQP_vkTwMcZv2r9d-zu80hIDVkEMypAAMKxr1Z9BQQ")
 
 @app.route("/summary",methods=["POST"])
 def summary():
@@ -16,23 +15,12 @@ def summary():
     user_input = data.get("text")
     print("Received text:", user_input)
 
-=======
-client = genai.Client(api_key="API KEY")
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "exit":
-        break
->>>>>>> 14cb1f146206a3151884c9178997c15d90756fb2
     response = client.models.generate_content(
         model="gemini-3.5-flash",
         contents=("Give a summary of this input:", user_input)
     )
     print("AI:", response.text)
-<<<<<<< HEAD
 
     return jsonify({"summary": response.text})
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=5000)
-=======
-    print('-'*100)
->>>>>>> 14cb1f146206a3151884c9178997c15d90756fb2
