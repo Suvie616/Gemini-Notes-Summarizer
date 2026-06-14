87fcf57547cdb6d508e76236fb86c5d49ae16b9a
Suvie_Suuwethaan <suvie616@gmail.com>
2026-06-14 01:29:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const paragraph = document.querySelector("#infoText");
 button.addEventListener("click",async (e) => {
    const text = textarea.value;
  try {
-   const req = await fetch ("https://your-ngrok-url.ngrok.io/summary", { 
+   const req = await fetch ("http://192.168.110.234:5000/summary", { 
     method: "POST",
     headers: {
       "Content-Type": "application/json"
