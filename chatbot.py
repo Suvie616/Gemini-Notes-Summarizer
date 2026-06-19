4fc24eba080e20d7906a2317add2707a501acc1c
Suvie_Suuwethaan <suvie616@gmail.com>
2026-06-19 05:48:06 -0700

    updated

--- chatbot.py
+++ chatbot.py
@@ -1,26 +1,35 @@
 from google import genai
 from flask import Flask,request,jsonify
+from flask import send_from_directory
+import os
 from flask_cors import CORS
+
 app = Flask(__name__)
 
-
-
-
 CORS(app,resources={r"/*": {"origins": "*"}})
-client = genai.Client(api_key="AQ.Ab8RN6LozQP_vkTwMcZv2r9d-zu80hIDVkEMypAAMKxr1Z9BQQ")
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+
+@app.route('/')
+def home():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:path>')
+def serve_file(path):
+    if os.path.exists(path):
+        return send_from_directory('.', path)
+    return "Not found", 404
 
 @app.route("/summary",methods=["POST"])
 def summary():
     data = request.get_json()
     user_input = data.get("text")
     print("Received text:", user_input)
-
     response = client.models.generate_content(
         model="gemini-3.1-flash-lite",
         contents=("Give a summary of this input:", user_input)
     )
     print("AI:", response.text)
-
     return jsonify({"summary": response.text})
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0",port=port)
--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const textarea = document.querySelector("#noteInput");
 const button = document.querySelector("#generateBtn");
 const paragraph = document.querySelector("#infoText");
-
 button.addEventListener("click",async (e) => {
    const text = textarea.value;
  try {
-   const req = await fetch ("http://192.168.110.234:5000/summary", { 
+    const req = await fetch("/summary", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ text})
-  });
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text })
+    });
   const data = await req.json();
   paragraph.textContent = data.summary;
   paragraph.classList.remove("hidden");
-
   } catch (error) {
     console.log("Fetch error:", error);
     paragraph.textContent = error
